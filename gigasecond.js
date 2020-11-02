0d3a1cb79916ae6a47a09815b4ac143898dd65ae
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-02 12:32:07 -0600

    already solved the problem; learned of the exisiting getTime() function; basically convert the date into time then add the MILLISECONDS; then convert back with new Date()

---
+++ gigasecond.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = (givenDate) => {
+  return new Date(givenDate.getTime() + 1000000000000);
+};
