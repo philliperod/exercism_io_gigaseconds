1dbb0989ce89900b68b3d3042c3d2a9598f6e6ba
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-07 11:54:08 -0600

    refactored by creating a variable to store the gigasecond and using math.pow

--- gigasecond.js
+++ gigasecond.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const gigaSecondInMilli = Math.pow(10, 12);
 
 export const gigasecond = (givenDate) => {
-  return new Date(givenDate.getTime() + 1000000000000);
+  return new Date(givenDate.getTime() + gigaSecondInMilli);
 };
